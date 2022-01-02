ad7bac51fcb46845599cfe65b15d8df16bf1c95d
chinocejas <nicocejas@outlook.com>
2022-01-02 19:02:24 -0300

    New structure

--- Functions/Information.cs
+++ Functions/Information.cs
@@ -7,21 +7,28 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using PersonalPage.Entities;
+using Entities.Functions;
+using Entities.Interfaces.Services;
 
 namespace PersonalPage.Functions
 {
     public class Information
     {
         private const string FunctionRoute = "info";
+        private readonly IPersonalInformationServices _personalInformationServices;
+
+        public Information(IPersonalInformationServices personalInformationServices)
+        {
+            _personalInformationServices = personalInformationServices;
+        }
 
         [FunctionName("Information")]
-        public static async Task<IActionResult> Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = FunctionRoute)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-            var personalInformation = new PersonalInfoDTO("Nico", "chinocejas");
+            var personalInformation = _personalInformationServices.GetPersonalInformationbyUserName("chino");
 
             return new OkObjectResult(personalInformation);
         }
